cd8867da6e1e3d108efff295badfc4f7eb7f895b
viduytsdn <69136147+viduytsdn@users.noreply.github.com>
2020-08-10 13:40:46 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-
+main()
 {
-
-double a,b;
-
-printf("enter a:");
-
-scanf("%f",&a);
-
-printf("enter b:"); scanf("%lf",&b);
-
-// Swapping
-
-//a=(initial a=intial_b)
-
-a=a-b;
-
-//b=(initial a-initial b)+initial b-initial a
-
-b=a+b;
-
-//a=initial _a-(initial _a-initial _b)=initial _b a=b-a;
-
-printf("after swapping a=!f\n", a); printf("after swapping b=!f\n",b);
-
-return 0;
+    printf("Select your favorite Food Item (*_*)\n");
+    printf("Pick an item : \n1. Pizza\n2. Burger\n3. Pasta\n4. French Fries\n5. Sandwich\n");
+    int choice=0;
+    scanf("%d", &choice);
+    switch(choice)
+    {
+    case 1:
+        printf("Food Item - Pizza Price - Rs 239 (*_*).");
+        break;
+    case 2:
+        printf("Food Item - Burger Price - Rs 129 (*_*).");
+        break;
+    case 3:
+        printf("Food Item - Pasta Price - Rs 179 (*_*).");
+        break;
+    case 4:
+        printf("Food Item - French Fries Price - Rs 99 (*_*).");
+        break;
+    case 5:
+        printf("Food Item - Sandwich Price - Rs 149 (*_*).");
+        break;
+    default : printf("\tInvalid choice...... (^..^)......");
+    }
 
 }
